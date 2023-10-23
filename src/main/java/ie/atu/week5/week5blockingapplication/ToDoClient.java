3683a1ea7b5cf4a2b4fea15200d2a5d0a585754d
Paddy Downey <G00392495@atu.ie>
2023-10-23 23:53:38 +0100

    Tried excercise

--- src/main/java/ie/atu/week5/week5blockingapplication/ToDoClient.java
+++ src/main/java/ie/atu/week5/week5blockingapplication/ToDoClient.java
@@ -3,10 +3,15 @@ package ie.atu.week5.week5blockingapplication;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 
 @FeignClient(name = "jsonplaceholder", url = "https://jsonplaceholder.typicode.com")
 public interface ToDoClient {
 
     @GetMapping("/todos/1")
     ToDoResponse fetchData();
+
+    @GetMapping("/todos/")
+    List<ToDoResponse> fetchAllData();
 }
